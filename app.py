3c2e5d8bbd7cad84a3f8c9233d036af0643f6631
loki1512 <100453418+loki1512@users.noreply.github.com>
2024-12-16 16:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,5 @@ def all_sessions():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no PORT environment variable is set
+    app.run(host="0.0.0.0", port=port)
